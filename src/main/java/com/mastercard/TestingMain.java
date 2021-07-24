39a43ade54e980da3283cde5d5b554dd256a9396
Lakshmi_Barthwal <Lakshmi_Barthwal@host.docker.internal>
2021-07-24 13:23:48 +0800

    Testing with pom

---
+++ src/main/java/com/mastercard/TestingMain.java
@@ -0,0 +1,11 @@
+package main.java.com.mastercard;
+
+
+public class TestingMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("testing");
+	}
+	
+}
