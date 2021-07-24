7a0dc5fea9f0307cdb3ebe3db66d69c53e6ff26e
Lakshmi_Barthwal <Lakshmi_Barthwal@host.docker.internal>
2021-07-24 13:59:56 +0800

    with mastercard skeleton

--- src/main/java/com/mastercard/TestingMain.java
+++ src/main/java/com/mastercard/TestingMain.java
@@ -1,11 +1,15 @@
-package main.java.com.mastercard;
+package com.mastercard;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TestingMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("testing");
 	}
-	
+	public boolean testword() {
+		return true;
+	}
 }
